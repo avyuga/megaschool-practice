7ac32fa5674de14c38e38f75bd3c852d39e9b61c
Anastasia <vnastav@gmail.com>
2024-02-17 16:53:29 +0300

    async task2

--- app/src/main/java/ru/sample/duckapp/MainActivity.kt
+++ app/src/main/java/ru/sample/duckapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.sample.duckapp
 
-//import android.R
+
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -10,37 +11,38 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.picasso.Picasso
-
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-
 import ru.sample.duckapp.domain.Duck
 import ru.sample.duckapp.infra.Api
+
 
 class MainActivity : AppCompatActivity() {
     private val api = Api.ducksApi
     private lateinit var url : String
-    private lateinit var DuckImage : ImageView
-    private lateinit var TextField: EditText
+    private lateinit var duckImage : ImageView
+    private lateinit var textField: EditText
+    private lateinit var button: Button
     private lateinit var currText: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        DuckImage = findViewById<View>(R.id.image) as ImageView
-        val button = findViewById<View>(R.id.button) as Button
-        TextField = findViewById<View>(R.id.text_field) as EditText
+        duckImage = findViewById<View>(R.id.image) as ImageView
+        button = findViewById<View>(R.id.button) as Button
+        textField = findViewById<View>(R.id.text_field) as EditText
 
         button.setOnClickListener {
-
-            currText = TextField.text.toString()
+            currText = textField.text.toString()
             if (currText == ""){
                 getDuck()
             }
             else{
-                getHttpDuck(currText.toInt())
+                getHttpDuckAsync("https://random-d.uk/api/v2/http/${currText.toInt()}")
+                textField.text.clear()
             }
         }
     }
@@ -51,29 +53,53 @@ class MainActivity : AppCompatActivity() {
                 if (response.isSuccessful){
                     val duck = response.body() as Duck
                     url = duck.url
-                    Picasso.get().load(url).resize(400, 0).centerCrop().into(DuckImage)
-
+                    Picasso.get().load(url).resize(400, 400).centerCrop().into(duckImage)
                 }
-
             }
             override fun onFailure(call: Call<Duck>, t: Throwable){
-                Picasso.get().load(R.drawable.sad_duck).resize(400, 0).centerCrop().into(DuckImage)
+                Picasso.get().load(R.drawable.sad_duck).resize(400, 0).centerCrop().into(duckImage)
                 Toast.makeText(this@MainActivity, "Error while loading random duck", Toast.LENGTH_LONG).show()
             }
         })
     }
 
+    // Метод без асинхронной обработки
     private fun getHttpDuck(code: Int){
         url = "https://random-d.uk/api/v2/http/${code}"
         Picasso.get()
             .load(url)
             .error(R.drawable.sad_duck)
             .resize(400, 0).centerCrop()
-            .into(DuckImage, object: com.squareup.picasso.Callback{
+            .into(duckImage, object: com.squareup.picasso.Callback{
                 override fun onSuccess(){}
                 override fun onError(e: Exception?){
                     Toast.makeText(this@MainActivity, "Wrong error code: $code", Toast.LENGTH_LONG).show()
                 }
             })
     }
+
+    private fun getHttpDuckAsync(url: String){
+        api.getHttpDuckApi(url).enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                Log.d("RESPONSE", response.toString())
+                if (response.isSuccessful){
+                    if (response.body() != null){
+                        val bmp = BitmapFactory.decodeStream(
+                            response.body()!!.byteStream()
+                        )
+                        duckImage.setImageBitmap(bmp)
+                    }
+                }
+                else{
+                    Picasso.get().load(R.drawable.sad_duck).resize(400, 0).centerCrop().into(duckImage)
+                    Toast.makeText(this@MainActivity, "Wrong error code", Toast.LENGTH_LONG).show()
+                }
+            }
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable){
+                Picasso.get().load(R.drawable.sad_duck).resize(400, 0).centerCrop().into(duckImage)
+                Toast.makeText(this@MainActivity, "Wrong error code", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
 }
--- app/src/main/java/ru/sample/duckapp/data/DucksApi.kt
+++ app/src/main/java/ru/sample/duckapp/data/DucksApi.kt
@@ -3,8 +3,14 @@ package ru.sample.duckapp.data
 import retrofit2.Call
 import retrofit2.http.GET
 import ru.sample.duckapp.domain.Duck
+import okhttp3.ResponseBody
+import retrofit2.http.Url
+
 
 interface DucksApi {
     @GET("random")
     fun getRandomDuck(): Call<Duck>
+
+    @GET
+    fun getHttpDuckApi(@Url url: String) : Call<ResponseBody>
 }
